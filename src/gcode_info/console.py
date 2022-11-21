96cbd2a3c6c3bbea1ee9aca5d04b65d51178de3b
Jordi Funollet <funollet@fastmail.fm>
2022-11-21 23:25:53 +0100

    --no-headers disable printing of headers

--- src/gcode_info/console.py
+++ src/gcode_info/console.py
@@ -9,12 +9,13 @@ from .gcodeinfo import parse_file
 @click.command()
 @click.version_option(version=__version__)
 @click.argument("files", type=click.Path(exists=True), nargs=-1)
-def main(files):
+@click.option("--headers/--no-headers", default=True)
+def main(files, headers):
     """Extract information from GCode files."""
 
     results = [parse_file(f) for f in files]
 
-    table = Table(box=None)
+    table = Table(box=None, show_header=headers)
     table.add_column("Duration", justify="right")
     table.add_column("Filament used", justify="right")
     table.add_column("Filename")
